df4931dde7c1e1d53360201f7a7737c75f4cd83f
Bautista <jabautista065@misena.edu.co>
2018-08-13 08:51:52 -0500

    cada item land

--- app/src/main/java/com/example/votosoft/simulacro/MainActivity.java
+++ app/src/main/java/com/example/votosoft/simulacro/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.votosoft.simulacro;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,14 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(MainActivity.this, lista_categorias.class);
+                startActivity(splash);
+            }
+        },3000);
     }
 
     public void onclick(View view){
-        Intent splash = new Intent(this, lista_categorias.class );
+        Intent splash = new Intent(this, nombre_item.class );
         startActivity(splash);
     }
 }
---
+++ app/src/main/java/com/example/votosoft/simulacro/nombre_item.java
@@ -0,0 +1,13 @@
+package com.example.votosoft.simulacro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nombre_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nombre_item);
+    }
+}
