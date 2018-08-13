ad4d85b3194eaacb8dde982218745c1cbc50ee13
Bautista <jabautista065@misena.edu.co>
2018-08-13 07:19:42 -0500

    splash imcompleto , imagenes

--- app/src/main/java/com/example/votosoft/simulacro/MainActivity.java
+++ app/src/main/java/com/example/votosoft/simulacro/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.votosoft.simulacro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    public void onclick(View view){
+        Intent splash = new Intent(this, lista_categorias.class );
+        startActivity(splash);
     }
 }
